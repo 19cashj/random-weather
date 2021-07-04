4477a68d930de87cec5f48e667fd5cddf4c9c73c
19cashj <67474732+19cashj@users.noreply.github.com>
2021-07-04 10:33:38 -0400

    Basic Hamburger Menu

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const loadingMsg = document.getElementById("loadingMsg");
 const mainContainer = document.querySelector(".main-container");
-navigator.geolocation.watchPosition(setCoords);
+const weatherImg1 = document.getElementsByClassName("weatherImg")[0];
+const weatherType = document.getElementById("weatherType");
+function geolocate() {
+    navigator.geolocation.watchPosition(setCoords);
+}
 async function setCoords(position) {
     let lat = position.coords.latitude;
     let log = position.coords.longitude;
@@ -15,36 +19,40 @@ async function setCoords(position) {
         const weatherData = await response.json();
         loadingMsg.classList.add("hidden");
         mainContainer.classList.remove("hidden");
-        setData(weatherData);
+        setMainData(weatherData);
     }
 }
 
-function setData(weatherData) {
+function setMainData(weatherData) {
     console.log(weatherData);
-    const currentDate = new Date();
     const date = document.getElementById("date");
     const location = document.getElementById("location");
-    const weatherImg1 = document.getElementsByClassName("weatherImg")[0];
-    const weatherType = document.getElementById("weatherType");
     const temperature = document.getElementById("temperature");
     const humidity = document.getElementById("humidity");
     const wind = document.getElementById("wind");
-    date.textContent = `${currentDate.getMonth()+1}/${currentDate.getDate()}/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}`;
-    temperature.textContent = `${Math.round(weatherData.main.temp)} F`;
+    const currentDate = new Date();
+    date.textContent = currentDate;
+    temperature.textContent = `${Math.round(weatherData.main.temp)}° F (${Math.round(weatherData.main.temp_max)}°/${Math.round(weatherData.main.temp_min)}°)`;
     location.textContent = weatherData.name;
-    weatherType.textContent = weatherData.weather[0].description;
-    switch (weatherData.weather[0].main) {
+    weatherType.textContent = `${weatherData.weather[0].description}`;
+    humidity.textContent = ` ${weatherData.main.humidity}%`;
+    wind.textContent = ` ${Math.round(weatherData.wind.speed)}mph`;
+    iconSelector(weatherData.weather[0].main, weatherImg1, currentDate);
+}
+
+function iconSelector(weatherDesc, img, currentDate) {
+    switch (weatherDesc) {
         case "Thunderstorm":
-            weatherImg1.classList.add("pe-is-w-mix-rainfall-2");
+            img.add("pe-is-w-mix-rainfall-2");
             break;
         case "Drizzle":
-            weatherImg1.classList.add("pe-is-w-drizzle");
+            img.classList.add("pe-is-w-drizzle");
             break;
         case "Rain":
-            weatherImg1.classList.add("pe-is-w-rain-1");
+            img.classList.add("pe-is-w-rain-1");
             break;
         case "Snow":
-            weatherImg1.classList.add("pe-is-w-snow");
+            img.classList.add("pe-is-w-snow");
             break;
         case "Mist":
         case "Smoke":
@@ -53,22 +61,37 @@ function setData(weatherData) {
         case "Sand":
         case "Ash":
         case "Squall":
-            weatherImg1.classList.add("pe-is-w-mist");
+            img.classList.add("pe-is-w-mist");
             break;
         case "Fog":
-            weatherImg1.classList.add("pe-is-w-fog-1");
+            img.classList.add("pe-is-w-fog-1");
             break;
         case "Tornado":
-            weatherImg1.classList.add("pe-is-w-tornado-1");
+            img.classList.add("pe-is-w-tornado-1");
             break;
         case "Clear":
-            weatherImg1.classList.add("pe-is-w-sun-1");
+            img.classList.add("pe-is-w-sun-1 pe-spin");
             break;
         case "Clouds":
-            weatherImg1.classList.add("pe-is-w-mostly-cloudy-1");
+            img.classList.add("pe-is-w-mostly-cloudy-1");
             break;
     }
-
-    humidity.textContent = ` ${weatherData.main.humidity}%`;
-    wind.textContent = ` ${weatherData.wind.speed}mph`;
 }
+function hamUIAnimate(state) {
+    if (state == 1) {
+        document.getElementById("hamUI").classList.remove("hamAnimateBack");
+        document.getElementById("hamUI").classList.remove("hidden");
+        document.getElementById("hamUI").classList.add("hamAnimateStart");
+    }
+    else {
+        document.getElementById("hamUI").classList.remove("hamAnimateStart");
+        document.getElementById("hamUI").classList.add("hamAnimateBack");
+        setTimeout(() => {
+            document.getElementById("hamUI").classList.add("hidden");
+        }, 500);
+    }
+}
+
+geolocate();
+    //Add if statements in cases for checking time of day to set the icon to the moon/sun
+    //Add a analog clock that detects the date and get hourly weather icons to attach to the hands of the clock to work as an hourly weather clock for the day
