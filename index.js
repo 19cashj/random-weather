8135aadbd38791bb35717b0ffaa7a6c63ed74faf
19cashj <67474732+19cashj@users.noreply.github.com>
2021-07-05 14:09:49 -0400

    Hourly Clock Placeholder, new API, weekly weather

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const loadingMsg = document.getElementById("loadingMsg");
 const mainContainer = document.querySelector(".main-container");
 const weatherImg1 = document.getElementsByClassName("weatherImg")[0];
 const weatherType = document.getElementById("weatherType");
+const canvas = document.getElementById("clockCanvas");
+const ctx = canvas.getContext(`2d`);
+const currentDate = new Date();
 function geolocate() {
     navigator.geolocation.watchPosition(setCoords);
 }
@@ -10,7 +13,7 @@ async function setCoords(position) {
     let log = position.coords.longitude;
     console.log(`Geolocated with Latitude: ${lat} Longitude: ${log}`)
     console.log("Fetching data from Openweathermap...")
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&units=imperial&appid=943f1223b9996ecae3cb1fe9233e975b`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${log}&exclude=minutely&units=imperial&appid=943f1223b9996ecae3cb1fe9233e975b`);
     if (!response.ok) {
         loadingMsg.innerText = (`Error ${response.status}`);
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -19,28 +22,53 @@ async function setCoords(position) {
         const weatherData = await response.json();
         loadingMsg.classList.add("hidden");
         mainContainer.classList.remove("hidden");
-        setMainData(weatherData);
+        setMainData(weatherData, currentDate);
+        setWeeklyForecast(weatherData, currentDate.getDay());
     }
 }
 
-function setMainData(weatherData) {
+function setMainData(weatherData, currentDate) {
     console.log(weatherData);
     const date = document.getElementById("date");
     const location = document.getElementById("location");
     const temperature = document.getElementById("temperature");
     const humidity = document.getElementById("humidity");
     const wind = document.getElementById("wind");
-    const currentDate = new Date();
     date.textContent = currentDate;
-    temperature.textContent = `${Math.round(weatherData.main.temp)}° F (${Math.round(weatherData.main.temp_max)}°/${Math.round(weatherData.main.temp_min)}°)`;
+    //
+    temperature.textContent = `${Math.round(weatherData.current.temp)}° F`;
     location.textContent = weatherData.name;
-    weatherType.textContent = `${weatherData.weather[0].description}`;
-    humidity.textContent = ` ${weatherData.main.humidity}%`;
-    wind.textContent = ` ${Math.round(weatherData.wind.speed)}mph`;
-    iconSelector(weatherData.weather[0].main, weatherImg1, currentDate);
+    weatherType.textContent = `${weatherData.current.weather[0].description}`;
+    humidity.textContent = ` ${weatherData.current.humidity}%`;
+    wind.textContent = ` ${Math.round(weatherData.current.wind_speed)}mph`;
+    iconSelector(weatherData.current.weather[0].main, weatherImg1, currentDate.getHours());
 }
 
-function iconSelector(weatherDesc, img, currentDate) {
+function setWeeklyForecast(weatherData, currentDay) {
+    const daysText = document.getElementsByClassName("daysText");
+    const daysImg = document.getElementsByClassName("weeklyWeatherImg");
+    const daysTemp = document.getElementsByClassName("highLow");
+    daysObject = {
+        0:"Sunday",
+        1:"Monday",
+        2:"Tuesday",
+        3:"Wednesday",
+        4:"Thursday",
+        5:"Friday",
+        6:"Saturday"
+    }
+    for (i=0, j=currentDay; i<8; i++, j++) {
+        if (j>6) {
+            j=0;
+        }
+        daysText[i].textContent = daysObject[j];
+        daysTemp[i].textContent = `${Math.round(weatherData.daily[i].temp.max)}°/${Math.round(weatherData.daily[i].temp.min)}°`;
+        iconSelector(weatherData.daily[i].weather[0].main, daysImg[i]);
+    }
+}
+
+function iconSelector(weatherDesc, img, currentHour) {
+    img.classList.remove("pe-spin");
     switch (weatherDesc) {
         case "Thunderstorm":
             img.add("pe-is-w-mix-rainfall-2");
@@ -70,7 +98,13 @@ function iconSelector(weatherDesc, img, currentDate) {
             img.classList.add("pe-is-w-tornado-1");
             break;
         case "Clear":
-            img.classList.add("pe-is-w-sun-1 pe-spin");
+            if (currentHour < 6 || currentHour > 19) {
+                img.classList.add("pe-is-w-moon-2");
+            }
+            else {
+                img.classList.add("pe-is-w-sun-1");
+                img.classList.add("pe-spin");
+            }
             break;
         case "Clouds":
             img.classList.add("pe-is-w-mostly-cloudy-1");
@@ -88,10 +122,47 @@ function hamUIAnimate(state) {
         document.getElementById("hamUI").classList.add("hamAnimateBack");
         setTimeout(() => {
             document.getElementById("hamUI").classList.add("hidden");
-        }, 500);
+        }, 400);
     }
 }
 
+function initialClockDraw() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillRect(115, 115, 20, 20);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.moveTo(125,250);
+    ctx.lineTo(125,0);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(0,125);
+    ctx.lineTo(250,125);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0,62);
+    ctx.lineTo(250,188);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(62,0);
+    ctx.lineTo(188,250);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(188,0);
+    ctx.lineTo(62,250);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0,188);
+    ctx.lineTo(250,62);
+    ctx.stroke();
+
+}
+
 geolocate();
-    //Add if statements in cases for checking time of day to set the icon to the moon/sun
-    //Add a analog clock that detects the date and get hourly weather icons to attach to the hands of the clock to work as an hourly weather clock for the day
+initialClockDraw();
+    //Add if statements in cases for checking time of day to set the icon to the moon/sun and if statements to check weather description and set icons
+    //Add weather clock next to current weather data. WIP
